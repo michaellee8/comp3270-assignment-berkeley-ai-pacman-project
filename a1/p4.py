9c5b319c4eb26a781e0799e541ca78ba55e67f9a
michaellee8 <ckmichael8@gmail.com>
2022-09-24 22:35:31 +0800

    feat: finish p4

--- a1/p4.py
+++ a1/p4.py
@@ -1,9 +1,26 @@
 import sys, parse, grader
+from parse import Problem
+import parse
+from collections import deque
+from typing import List
+import heapq
+
 
 def greedy_search(problem):
-    #Your p4 code here
-    solution = 'S B D C\nS C G'
-    return solution
+    pq = []
+    exploration_order = []
+    exploration_order_added = set()
+    heapq.heappush(pq, (problem.heuristic[problem.start_state], [problem.start_state]))
+    while pq:
+        current_cost, current_path = heapq.heappop(pq)
+        if current_path[-1] in problem.goal_state:
+            return f"{' '.join(exploration_order)}\n{' '.join(current_path)}"
+        if current_path[-1] not in exploration_order_added:
+            exploration_order.append(current_path[-1])
+            exploration_order_added.add(current_path[-1])
+        for next_node in problem.state_transitions[current_path[-1]]:
+            heapq.heappush(pq, (problem.heuristic[next_node], current_path + [next_node]))
+
 
 if __name__ == "__main__":
     test_case_id = int(sys.argv[1])
