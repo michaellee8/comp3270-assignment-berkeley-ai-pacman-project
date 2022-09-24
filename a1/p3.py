bad91308364dfbee6f2057559c57d095f42e63b1
michaellee8 <ckmichael8@gmail.com>
2022-09-24 21:54:11 +0800

    feat: finish p3

--- a1/p3.py
+++ a1/p3.py
@@ -1,9 +1,26 @@
 import sys, parse, grader
+from parse import Problem
+import parse
+from collections import deque
+from typing import List
+import heapq
 
-def ucs_search(problem):
-    #Your p3 code here
-    solution = 'S D B C\nS C G'
-    return solution
+
+def ucs_search(problem: Problem):
+    pq = []
+    exploration_order = []
+    exploration_order_added = set()
+    heapq.heappush(pq, (0, [problem.start_state]))
+    while pq:
+        total_cost, path = heapq.heappop(pq)
+        if path[-1] in problem.goal_state:
+            return f"{' '.join(exploration_order)}\n{' '.join(path)}"
+        if path[-1] not in exploration_order_added:
+            exploration_order.append(path[-1])
+            exploration_order_added.add(path[-1])
+        for next_node, cost in problem.state_transitions[path[-1]].items():
+            heapq.heappush(pq, (cost + total_cost, path + [next_node]))
+
 
 if __name__ == "__main__":
     test_case_id = int(sys.argv[1])
