a56dde06d7818d1edddc6b8b523bf76ac5cb347b
michaellee8 <ckmichael8@gmail.com>
2022-10-15 23:51:56 +0800

    chore: wip

--- a2/p1.py
+++ a2/p1.py
@@ -1,9 +1,14 @@
 import sys, random, grader, parse
+from parse import Problem
 
-def random_play_single_ghost(problem):
-    #Your p1 code here
+
+def random_play_single_ghost(problem: Problem):
+    random.seed(problem.seed, version=1)
+
+    # Your p1 code here
     solution = ''
     return solution
+
 
 if __name__ == "__main__":
     test_case_id = int(sys.argv[1])
